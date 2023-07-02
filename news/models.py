7f1544ada3f4e2dd8a58f49ec0c1d3309e79053f
Shaf8808 <shadmankhan08@gmail.com>
2023-07-02 13:05:08 +0000

    Make some changes to my data models

--- news/models.py
+++ news/models.py
@@ -9,38 +9,38 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Article(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_articles")
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_articles"
+    )
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='article_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='article_likes', blank=True)
 
     class Meta:
-        # Order articles in descending order based on
-        # the created on field
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"{self.title}"
+        return self.title
 
     def number_of_likes(self):
         return self.likes.count()
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=100)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name="comments")
+    name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
-        # Order articles in descending order based on
-        # the created on field
         ordering = ["created_on"]
 
     def __str__(self):
