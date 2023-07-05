8cd3f9dbc189ab80b2cccecffffc64629b36d0eb
Shaf8808 <shadmankhan08@gmail.com>
2023-07-05 09:41:06 +0000

    Add attributes such as list filter and search fields to article admin class.
    Create and register comment admin class in order to display them in the Django admin page

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    search_fields = ('name', 'email', 'body')
+    list_display = ('name', 'body', 'article', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
