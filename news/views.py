581afbdc4b765fdde12824e87a3b1ccd1d039878
Shaf8808 <shadmankhan08@gmail.com>
2023-07-13 09:03:01 +0000

    Lay out the structure of my homepage and how all of the data will be presented to the user.
    Work on custom class type view so both Release and Article data will be displayed on the homepage.

--- news/views.py
+++ news/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Article, Release
+from .models import Article, Release, Review
 
 
 class ArticleList(generic.ListView):
-    model = Article
-    queryset = Article.objects.filter(status=1).order_by('-created_on')
+    context_object_name = 'article_list'
     template_name = 'index.html'
-
+    queryset = Article.objects.filter(status=1).order_by('-created_on')
+   
     def get_context_data(self, *args, **kwargs):
         context = super(ArticleList, self).get_context_data(*args, **kwargs)
         context['release_list'] = Release.objects.order_by("-release_date")
